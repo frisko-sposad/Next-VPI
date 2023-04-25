ffcef667483615d24c997778f8623a66072d6639
frisko-sposad <safiko@ya.ru>
2023-04-25 22:03:32 +0300

    added menu

--- pages/battle-calculator/index.tsx
+++ pages/battle-calculator/index.tsx
@@ -6,6 +6,7 @@ import PlayerArmy from '../../components/player-army';
 
 import { battle, ParseData } from '../../function/battle';
 import { getParseData } from '../../function/get-parse-data';
+import Link from 'next/link';
 
 const App = () => {
   const [unitsData, setUnitsData] = useState({} as unknown as ParseData);
@@ -35,6 +36,32 @@ const App = () => {
 
   return (
     <>
+      <nav
+        className="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+        data-te-navbar-ref
+      >
+        <div className="flex w-full flex-wrap items-center justify-between px-3">
+          <div>
+            <ul className="mr-auto flex flex-col lg:flex-row" data-te-navbar-nav-ref>
+              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link href="/battle-calculator">
+                  <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90">
+                    <h2>battle-calculator</h2>
+                  </a>
+                </Link>
+              </li>
+              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link href="/units">
+                  <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90">
+                    <h2>Units</h2>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex justify-center">
--- pages/units/index.tsx
+++ pages/units/index.tsx
@@ -3,6 +3,7 @@ import { useExpanded, useTable } from 'react-table';
 import { unitsData } from '../../public/database/units-data';
 import { fortificationData } from '../../public/database/fortification-data';
 import { heroesData } from '../../public/database/heroes-data';
+import Link from 'next/link';
 
 const Table = ({ columns: userColumns, data }: any) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
@@ -291,19 +292,46 @@ const StatsTable = () => {
   );
 
   return (
-    <div className="flex justify-center">
-      <div className="flex-col">
-        <div className="pb-10">
-          <Table columns={columnsUnits} data={dataUnits} />
+    <>
+      <nav
+        className="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+        data-te-navbar-ref
+      >
+        <div className="flex w-full flex-wrap items-center justify-between px-3">
+          <div>
+            <ul className="mr-auto flex flex-col lg:flex-row" data-te-navbar-nav-ref>
+              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link href="/battle-calculator">
+                  <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90">
+                    <h2>battle-calculator</h2>
+                  </a>
+                </Link>
+              </li>
+              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link href="/units">
+                  <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90">
+                    <h2>Units</h2>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="pb-10">
-          <Table columns={columnsHeroes} data={dataHeroes} />
-        </div>
-        <div className="pb-10">
-          <Table columns={columnsFortification} data={dataFortification} />
+      </nav>
+      <div className="flex justify-center">
+        <div className="flex-col">
+          <div className="pt-5">
+            <Table columns={columnsUnits} data={dataUnits} />
+          </div>
+          <div className="pt-5">
+            <Table columns={columnsHeroes} data={dataHeroes} />
+          </div>
+          <div className="pt-5">
+            <Table columns={columnsFortification} data={dataFortification} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

b05354f396212a86bb6f44866c509380eb267894
frisko-sposad <safiko@ya.ru>
2023-10-17 00:01:19 +0300

    added graph location

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 
 const UserInfo = () => {
   const [dataUsers, setDataUsers] = useState([]);
+  console.log(dataUsers);
 
   const router = useRouter();
   console.log(router.query.id);
@@ -47,6 +48,10 @@ const UserInfo = () => {
         Header: 'Номер игры',
         accessor: 'games_id',
       },
+      {
+        Header: 'Перемещение',
+        accessor: 'concat_loc',
+      },
     ],
     [],
   );
@@ -56,9 +61,15 @@ const UserInfo = () => {
       <Header />
       <div className="flex justify-center">
         <div className="flex-col">
-          <div>Игрок: {dataUsers[0].login}</div>
+          <div>Игрок: {dataUsers[0] && dataUsers[0].login}</div>
           <div className="pt-5">
             <Table columns={columnsUsers} data={dataUsers} />
+            <select name="ids" id="ids">
+              <option value="1">Id 1</option>
+              <option value="2">Id 2</option>
+              <option value="3">Id 3</option>
+              <option value="4">Id 4</option>
+            </select>
           </div>
         </div>
       </div>
